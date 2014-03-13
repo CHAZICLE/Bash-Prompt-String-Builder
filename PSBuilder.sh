45abd59f6c3148f61591e1fdbfb2e56209f5439a
CHAZICLE <chazicle@gmail.com>
2014-03-13 14:55:23 +0000

    Used proper color codes

--- PSBuilder.sh
+++ PSBuilder.sh
@@ -5,28 +5,28 @@ rc=$1
 echo -n -e ┌\(
 # Print Last Command Sucessful
 if [ $rc -eq 0 ]; then
-        echo -en \\\e[01\;32m✓
+        echo -en \\\033[01\;32m✓
 else
-        echo -en \\\e[01\;31m✗
+        echo -en \\\033[01\;31m✗
 fi
-echo -e \ $rc\\\e[0m\)─\(\\\e[34m$(date)\\\e[0m\)
+echo -e \ $rc\\\033[0m\)─\(\\\033[34m$(date)\\\033[0m\)
 ##################
 # Seperation Bar #
 ##################
 hostnamecolor=$(hostname | od | tr ' ' '\n' | awk '{total = total + $1}END{print 30 + (total % 6)}')
-echo -e "├\e[${hostnamecolor}m████████████████████████████████████████████████████████\e[0m"
+echo -e "├\033[${hostnamecolor}m████████████████████████████████████████████████████████\033[0m"
 ############
 # Git Info #
 ############
 git branch &> /dev/null
 if [ $? -eq 0 ]; then
-	echo -ne ├\(\\\e[36mGIT\:
+	echo -ne ├\(\\\033[36mGIT\:
 	echo -n `git branch | cut -b 3-`
-	echo -ne \\\e[0m\)─
+	echo -ne \\\033[0m\)─
 	
 	echo `git status` | grep "nothing to commit" > /dev/null 2>&1
 	if [ $? -eq 0 ]; then
-		echo -ne \(\\\e[32mNothing to commit\\\e[0m\)
+		echo -ne \(\\\033[32mNothing to commit\\\033[0m\)
 	else
 		echo -n "(`git status -s | grep "?? " | wc -l` Untracked, `git status -s | grep "A  " | wc -l` Added, `git status -s | grep " M " | wc -l` Modified)"
 	fi
@@ -37,13 +37,13 @@ fi
 #######################
 echo -ne ├\(
 if [[ $EUID -ne 0 ]]; then
-        echo -en \\\e[35m$USER
+        echo -en \\\033[35m$USER
 else
-        echo -en \\\e[31mROOT
+        echo -en \\\033[31mROOT
 fi
-echo -ne \\\e[39m@\\\e[32m$HOSTNAME\\\e[0m\)
-echo -ne ─\(\\\e[36m$(ls -1 | wc -l | sed 's: ::g') files\@$(ls -lah | grep -m 1 total | sed 's/total //')\\\e[0m\)
-echo -e ─\\\e[36m$PWD\\\e[0m
+echo -ne \\\033[39m@\\\033[32m$HOSTNAME\\\033[0m\)
+echo -ne ─\(\\\033[36m$(ls -1 | wc -l | sed 's: ::g') files\@$(ls -lah | grep -m 1 total | sed 's/total //')\\\033[0m\)
+echo -e ─\\\033[36m$PWD\\\033[0m
 ############
 #Final Line#
 ############
